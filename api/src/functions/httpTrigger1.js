5123134dcd32fa2c17f2c3fd53b4d63d2dcc16ca
OGR-67 <roosolivier@gmail.com>
2024-10-11 11:54:49 +0200

    Add staticwebapp.config.json

--- api/src/functions/httpTrigger1.js
+++ api/src/functions/httpTrigger1.js
@@ -4,6 +4,7 @@ app.http('httpTrigger1', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
+        request.headers.get("")
         context.log(`Http function processed request for url "${request.url}"`);
 
         const name = request.query.get('name') || await request.text() || 'world';
